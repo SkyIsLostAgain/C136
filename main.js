39321d34326f57a8f79ea5ff078732caa7ea8a60
SkyIsLostAgain <136880731+SkyIsLostAgain@users.noreply.github.com>
2024-04-28 14:04:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+status = "";
+objects = [];
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -12,11 +14,22 @@ function draw(){
     image(video, 0, 0, 480, 380);
     if(status != ""){
         objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            document.getElementById("numberofobjects").innerhtml = "Number Of Objects Detected : " + objects;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15)
+        }
     }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    noFill();
+    stroke("#FF0000");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 }
 function modelLoaded(){
     console.log("Model Loaded!");
@@ -30,4 +43,5 @@ function gotResult(error, results ){
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
