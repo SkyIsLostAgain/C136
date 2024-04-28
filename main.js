fce73898dfe1a6c2020fd740609c51ae0d4744f3
SkyIsLostAgain <136880731+SkyIsLostAgain@users.noreply.github.com>
2024-04-28 13:47:34 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+    }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
@@ -21,4 +24,10 @@ function modelLoaded(){
     video.loop();
     video.speed(2);
     video.volume(0.5);
+}
+function gotResult(error, results ){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
